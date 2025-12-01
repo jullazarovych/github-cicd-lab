dabdbd5582f695e703d4aec48e2d5b2f355a4703
jullazarovych <juliala125@gmail.com>
2025-12-01 13:38:32 +0200

    second try

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def add(a, b):
     result = a + b
     return jsonify({"operation": "add", "a": a, "b": b, "result": result})
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000)
